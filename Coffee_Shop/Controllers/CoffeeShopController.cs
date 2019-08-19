36509cf5fed2d915e151418c38fb16362866ca49
Savanah Conrad <sibgym@yahoo.com>
2019-08-18 21:23:26 -0400

    First Commit

---
+++ Coffee_Shop/Controllers/CoffeeShopController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Coffee_Shop.Models;
+
+namespace Coffee_Shop.Controllers
+{
+    public class CoffeeShopController : Controller 
+    {
+        public IActionResult Index(RegisterUser user)
+        {
+            return View(user);
+        }
+
+        // Get information from our user
+        [HttpGet]
+        public IActionResult UserForm()
+        {
+            return View(new RegisterUser());
+        }
+
+        // sending user to RegisterUser action
+        [HttpPost] 
+        public IActionResult UserForm(RegisterUser user)
+        {
+            return RedirectToAction("Index", user);
+        }
+
+        public IActionResult RegisterUser(RegisterUser user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ Coffee_Shop/Models/RegisterUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Coffee_Shop.Models
+{
+    public class RegisterUser
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
