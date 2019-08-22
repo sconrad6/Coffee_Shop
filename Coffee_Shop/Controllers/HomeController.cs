7810d1167e6e287a8838c28557fb8cd43fa4fa20
Savanah Conrad <sibgym@yahoo.com>
2019-08-22 16:24:44 -0400

    Refactored

--- Coffee_Shop/Controllers/HomeController.cs
+++ Coffee_Shop/Controllers/HomeController.cs
@@ -5,46 +5,65 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Coffee_Shop.Models;
-using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
 
 namespace Coffee_Shop.Controllers
 {
     public class HomeController : Controller
     {
+        List<RegisterUser> users = new List<RegisterUser>();
+
+        public IActionResult Index(RegisterUser user)
+        {
+            return View(user);
+        }
+
+        public IActionResult UserWelcome(RegisterUser user)
+        {
+            return View(user);
+        }
+
+        // Get information from our user
+        [HttpGet]
+        public IActionResult UserForm()
+        {
+            return View(new RegisterUser());
+            
+        }
+       
+        // sending user to RegisterUser action
+        [HttpPost]
+        public IActionResult UserForm(RegisterUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                string userJson = HttpContext.Session.GetString("UserSession");
+                if (userJson != null)
+                {
+                    users = JsonConvert.DeserializeObject<List<RegisterUser>>(userJson);
+                }
+                users.Add(user);
+                HttpContext.Session.SetString("UserSession", JsonConvert.SerializeObject(users));
+                //return View("ListUsers", users);
+                return RedirectToAction("UserWelcome", user);
+            }
+            return View("UserForm", user);
+        }
+
+        public IActionResult UserView(RegisterUser user)
+        {
+            return View(user);
+        }
+
         public IActionResult ListUsers()
         {
-            return View();
+            string userJson = HttpContext.Session.GetString("UserSession");
+            if (userJson != null)
+            {
+                users = JsonConvert.DeserializeObject<List<RegisterUser>>(userJson);
+            }
+            return View(users);
         }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        //public IActionResult RegisterUserList()
-        //{
-        //    string userJson = HttpContext.Session.GetString("UserSession");
-        //    if (userJson != null)
-        //    {
-        //        users = JsonConvert.DeserializeObject<List<RegisterUser>>(userJson);
-        //    }
-        //    return View("ListUsers", users);
-        //}
-
-        [HttpPost]
-        // this will save the user's information in a cookie
-        public IActionResult SaveUser(RegisterUser user)
-        {
-            CookieOptions option = new CookieOptions();
-            Response.Cookies.Append("UserName", user.UserName, option);
-            Response.Cookies.Append("FirstName", user.FirstName, option);
-            Response.Cookies.Append("LastName", user.LastName, option);
-            Response.Cookies.Append("Email", user.Email, option);
-            Response.Cookies.Append("Age", user.Age.ToString(), option);
-
-            return RedirectToAction("SaveUser", user);
-        }
-
     }
 }
